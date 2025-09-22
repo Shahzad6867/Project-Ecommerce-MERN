6e57796c339b7da3589f71725068de91381fd3c2
Shahzad6867 <muhammadshahzad6867@gmail.com>
2025-09-22 21:53:24 +0400

    Javascript File added for the Validation and some mofifications in the head of ejs

---
+++ public/javascript/registrationValidate.js
@@ -0,0 +1,81 @@
+const passwordReg = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$&]).{8,20}$/;
+const emailReg = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+const nameReg = /^[A-Za-z\s]+$/;
+
+var rName = document.getElementById("reg-name");
+var rPassword = document.getElementById("reg-password");
+var rCpassword = document.getElementById("reg-confirm-password");
+var rEmail = document.getElementById("reg-email");
+var rTerms = document.getElementById("terms");
+
+rName.addEventListener("input", () => {
+  if (rName.value.length < 5 || !nameReg.test(rName.value)) {
+    rName.style.border = "2px solid red";
+    document.getElementById("reg-name-p").style.color = "red";
+  } else {
+    rName.style.border = "2px solid green";
+    document.getElementById("reg-name-p").style.color = "";
+  }
+});
+
+rEmail.addEventListener("input", () => {
+  if (!emailReg.test(rEmail.value)) {
+    rEmail.style.border = "2px solid red";
+    document.getElementById("reg-email-p").style.color = "red";
+  } else {
+    rEmail.style.border = "2px solid green";
+    document.getElementById("reg-email-p").style.color = "";
+  }
+});
+
+rPassword.addEventListener("input", () => {
+  if (!passwordReg.test(rPassword.value)) {
+    rPassword.style.border = "2px solid red";
+    document.getElementById("reg-password-p").style.color = "red";
+  } else {
+    rPassword.style.border = "2px solid green";
+    document.getElementById("reg-password-p").style.color = "";
+  }
+});
+
+rCpassword.addEventListener("input", () => {
+  if (rCpassword.value !== rPassword.value) {
+    rCpassword.style.border = "2px solid red";
+    document.getElementById("reg-confirmPassword-p").style.color = "red";
+  } else {
+    rCpassword.style.border = "2px solid green";
+    document.getElementById("reg-confirmPassword-p").style.color = "";
+  }
+});
+
+rTerms.addEventListener("change", () => {
+  if (!rTerms.checked) {
+    document.getElementById("terms-text").style.color = "red";
+  } else {
+    document.getElementById("terms-text").style.color = "";
+  }
+});
+
+function validate() {
+  if (
+    rName.style.border !== "2px solid green" ||
+    rEmail.style.border !== "2px solid green" ||
+    rPassword.style.border !== "2px solid green" ||
+    rCpassword.style.border !== "2px solid green"
+  ) {
+    iziToast.error({
+      title: "Error",
+      message: "Please fill all the Required Fields",
+      position: "topRight",
+    });
+    return false;
+  } else if (!rTerms.checked) {
+    iziToast.error({
+      title: "Error",
+      message: "Please Accept the Terms & Conditions!",
+      position: "topRight",
+    });
+    return false;
+  }
+  return true;
+}
