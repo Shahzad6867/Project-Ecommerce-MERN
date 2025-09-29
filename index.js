93ba9124c9bf58e2e72cf48a4e037cc72eaf9fe0
Shahzad6867 <muhammadshahzad6867@gmail.com>
2025-09-29 14:42:01 +0400

    Style changes towards otp onput box on smaller devices

--- index.js
+++
@@ -1,8 +0,0 @@
-let otpCreatedTimeString = NaN
- let otpCreatedTime = new Date(otpCreatedTimeString)   
- console.log(otpCreatedTime)  
- let now = new Date()
- let countdown = (otpCreatedTime.getTime() - now.getTime()) / 1000
-
-
- console.log(countdown)
